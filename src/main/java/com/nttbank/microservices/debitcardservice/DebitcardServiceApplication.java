3b2feacfdac7ca270e042e104dd5fe9167209f89
Jesus Alberto Fernandez Malpartida <jfernmal@emeal.nttdata.com>
2025-01-14 11:09:50 -0500

    Intial commit: base project for ms debitcard-service

---
+++ src/main/java/com/nttbank/microservices/debitcardservice/DebitcardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttbank.microservices.debitcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitcardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitcardServiceApplication.class, args);
+	}
+
+}
